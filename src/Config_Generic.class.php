ddbf21b5c5e486a2f1df2c2c89a6eb9128989d4c
Lorenzo L. Romero <lorenzolrom@gmail.com>
2019-04-10 23:22:14 -0400

    Adding generic configuration file.

---
+++ src/Config_Generic.class.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * LLR Technologies & Associated Services
+ * Information Systems Development
+ *
+ * WebNOC Dashboards
+ *
+ * User: lromero
+ * Date: 4/05/2019
+ * Time: 6:42 PM
+ */
+
+
+class Config_Generic
+{
+    const OPTIONS = array(
+        'icUrl' => "https://infocentral.url/",
+        'icPort' => "443",
+        'icSecret' => "INFOCENTRAL_SECRET"
+    );
+}
